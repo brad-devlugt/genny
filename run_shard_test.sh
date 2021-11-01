903884bd4cc9692603cedf4bc38b00e6b2764840
Brad de Vlugt <brad.devlugt@mongodb.com>
2021-11-01 13:11:13 -0400

    Backup of work

--- run_shard_test.sh
+++ run_shard_test.sh
@@ -7,39 +7,48 @@ function teardown() {
 
 workload="$1"
 
-
 teardown
 
 rm -rf dbs/
 
 echo "Starting backing mongod..."
-mkdir -p dbs/logs
-mkdir -p dbs/data
-mongod --shardsvr --replSet "shard-replset" --dbpath ./dbs/data --logpath ./dbs/logs/mongod.log --port 27001 --fork
 
+for i in {1..3}
+do
+    datapath="dbs/data_${i}"
+    logpath="dbs/logs_${i}"
+    port="2700${i}"
+    mkdir -p $datapath
+    mkdir -p $logpath
+    mongod --shardsvr --replSet "shard-replset-${i}" --dbpath $datapath --logpath $logpath/mongod.log --port $port --fork
+    mongosh --eval 'rs.initiate()' "mongodb://localhost:${port}"
+done
 
-mongosh --eval 'rs.initiate()' "mongodb://localhost:27001"
 
 echo "Starting config server..."
 mkdir -p dbs/config_data
 mkdir -p dbs/config_logs
-mongod --configsvr --replSet "config-replset" --dbpath ./dbs/config_data --logpath ./dbs/config_logs/mongod.log --port 27002 --fork
+mongod --configsvr --replSet "config-replset" --dbpath ./dbs/config_data --logpath ./dbs/config_logs/mongod.log --port 27000 --fork
 
-mongosh --eval 'rs.initiate()' "mongodb://localhost:27002"
+mongosh --eval 'rs.initiate()' "mongodb://localhost:27000"
 
 
 echo "Starting mongos"
 mkdir -p dbs/mongos_data
 mkdir -p dbs/mongos_logs
-mongos --configdb "config-replset/localhost:27002" --port 27003 --logpath ./dbs/mongos_logs/mongos.log --fork
+mongos --configdb "config-replset/localhost:27000" --port 27015 --logpath ./dbs/mongos_logs/mongos.log --fork
 
 
-mongosh --eval 'sh.addShard( "shard-replset/localhost:27001")' "mongodb://localhost:27003"
+for i in {1..3}
+do
+    shard_str="shard-replset-${i}/localhost:2700${i}"
+    mongosh --eval "sh.addShard(\"${shard_str}\")" "mongodb://localhost:27015"
+done
 
-./run-genny workload -- run -w "$workload" -u "mongodb://localhost:27003"
+./run-genny workload -- run -w "$workload" -u "mongodb://localhost:27015"
 
 ./run-genny translate ./build/WorkloadOutput/CedarMetrics/ -o ./build/WorkloadOutput/CedarMetrics/out.ftdc
 
-t2 ./build/WorkloadOutput/CedarMetrics/out.ftdc ./dbs/data/diagnostic.data/
+t2 ./build/WorkloadOutput/CedarMetrics/out.ftdc
 
 teardown
